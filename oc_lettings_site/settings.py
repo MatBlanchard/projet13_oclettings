ad7da4b1e6ee7981947ad57acaf10267f11653d9
MatBlanchard <matblanchard.pro@gmail.com>
2023-03-27 12:31:31 +0200

    Ajout flake8-html

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,8 +15,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['oclettings-site.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
